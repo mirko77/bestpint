1dd132aa4327d6e2c60ef7fcc095dcfc1783bdff
Mirko Menegazzo <mirko.menegazzo@gmail.com>
2015-06-22 14:23:24 +0100

    Added clusters, still need to find a way to pass in data for infobox

--- js/app_modules/Bestpint.renderBingMaps.js
+++ js/app_modules/Bestpint.renderBingMaps.js
@@ -79,7 +79,7 @@ Bestpint.renderBingMaps = function () {
                     current_lat = Bestpint.entries[i].ecplus_Place_ctrl3.latitude;
                     current_long = Bestpint.entries[i].ecplus_Place_ctrl3.longitude;
 
-                    data.push({latitude: +current_lat, longitude: +current_long});
+                    data.push({latitude: +current_lat, longitude: +current_long, id: i});
 
                     /*************************************************
                      Following code is ok if we do not need clusters
@@ -92,7 +92,7 @@ Bestpint.renderBingMaps = function () {
                     //// Create the infobox for the pushpin
                     //infowindow = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(current_lat, current_long),null);
                     //infowindow.setOptions({visible: false});
-                    ////infowindow.setHtmlContent('<div class="bestpint-infobox"></div><div>');
+                    ////infowindow.setHtmlContent('<div class="bestpint-infobox"></div>');
                     //infowindow_layer.push(infowindow);
                     //
                     //// Add handler for the pushpin click event.
@@ -121,6 +121,7 @@ Bestpint.renderBingMaps = function () {
                                 width: 32,
                                 height: 32
                             });
+
                         }
                         else {
                             pushpin.setOptions({
@@ -131,39 +132,38 @@ Bestpint.renderBingMaps = function () {
                             });
                         }
 
-
-                        Microsoft.Maps.Events.addHandler(pushpin, 'mouseover', function () {
-                            displayInfoBox(pushpin, cluster);
+                        Microsoft.Maps.Events.addHandler(pushpin, 'click', function () {
+                            if (cluster.length === 1) {
+                                displayInfoBox(pushpin, cluster);
+                            }
                         });
 
-                        Microsoft.Maps.Events.addHandler(pushpin, 'mouseout', hideInfoBox);
+                        //Microsoft.Maps.Events.addHandler(pushpin, 'mouseout', hideInfoBox);
 
 
                         Microsoft.Maps.Events.addHandler(map, 'viewchangeend', function (e) {
-
                             //when zoom is level 3, all markers disappear?
                             //solution, change MIN_ZOOM = 3, (it was 2), in pin_clusterer.js
-
                             console.log(map.getZoom());
-
-
                         });
-
                     }
                 });
+           // pinInfobox.setHtmlContent('<div class="bestpint-infobox"></div>');
 
             map.entities.push(pinInfobox);
             pinClusterer.cluster(data);
             map.setView({center: new Microsoft.Maps.Location(0, 0), zoom: 2});
 
-
             var displayInfoBox = function displayInfoBox(pushpin, cluster) {
+                //pinInfobox.setLocation(pushpin.getLocation());
+                //pinInfobox.setOptions({
+                //    title: 'Pushpin',
+                //    description: 'This pin is within a cluster of ' + cluster.locations.length + ' pins.',
+                //    visible: true
+                //});
+                console.log(pushpin.getId());
+                pinInfobox.setOptions({description: pushpin.Description, visible: true, showPointer: false});
                 pinInfobox.setLocation(pushpin.getLocation());
-                pinInfobox.setOptions({
-                    title: 'Pushpin',
-                    description: 'This pin is within a cluster of ' + cluster.locations.length + ' pins.',
-                    visible: true
-                });
             };
 
             var hideInfoBox = function hideInfoBox() {
@@ -182,17 +182,10 @@ Bestpint.renderBingMaps = function () {
                 infowindow.setOptions({description: e.target.Description, visible: true, showPointer: false});
                 infowindow.setLocation(e.target.getLocation());
             }
-
             function hideInfobox(e) {
                 infowindow.setOptions({visible: false});
             }
-
             Bestpint.fakeloader.fadeOut();
-
-
         }
-
     };
-
-
 };
