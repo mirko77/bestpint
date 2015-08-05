0c0942d6a05757af9d83f2004260a8eb31516470
Mirko Menegazzo <mirko.menegazzo@gmail.com>
2015-08-05 13:09:07 +0100

    Update Bestpint.getData.js

--- js/app_modules/Bestpint.getData.js
+++ js/app_modules/Bestpint.getData.js
@@ -5,23 +5,6 @@ Bestpint.getData = function () {
     var deferred = new $.Deferred();
     var url = 'http://plus.epicollect.net/Bestpint/Beer.json';
 
-    //try a cors request to epicollect server using yql https://developer.yahoo.com/yql
-    //$.getJSON("http://query.yahooapis.com/v1/public/yql",
-    //    {
-    //        q: "select * from json where url='http://plus.epicollect.net/Bestpint/Beer.json'",
-    //        format: "json"
-    //    },
-    //    function (data, status) {
-    //        //if the proxy is down for some reasons, show error message
-    //        if (data.query.results) {
-    //            //console.log(data.query.results.json.json);
-    //            deferred.resolve(data.query.results.json.json);
-    //        }
-    //        else {
-    //            deferred.reject(status);
-    //        }
-    //    });
-
     $.ajax({
         url: 'http://query.yahooapis.com/v1/public/yql',
         data: {
